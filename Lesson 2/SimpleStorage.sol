068be2fe8e729deb5bdb57b1cc1d31a4df60ec20
George <110556745+Salutix@users.noreply.github.com>
2023-01-27 23:23:25 +0300

    Update SimpleStorage.sol

--- Lesson 2/SimpleStorage.sol
+++ Lesson 2/SimpleStorage.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8; // all versions between work ^0.8.7 (most stable versions and higher)
+// also possible f.e. >=0.8.0 <0.9.0;
 
 /* Boolean – true/false, 
 uint – whole numbers non-negative, 
